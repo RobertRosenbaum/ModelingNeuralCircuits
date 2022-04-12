8d9159ee83a56b7765c04fc4a3803daba6d6019b
RobertRosenbaum <rjrosenbaum@gmail.com>
2022-04-12 11:44:14 +0200

    Uploaded external data and code files
    Test running and loading of files from local machine and Colab

---
+++ GlobalSettings.py
@@ -0,0 +1,19 @@
+# Import packages that are used throughout the book
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+# This makes sure that figures show up in the notebooks
+%matplotlib inline
+
+
+# This makes plots look nice
+sns.set(context='paper',style='white',font_scale=1.5, rc={"lines.linewidth": 2.5})
+sns.set_palette('muted')
+
+
+# Set this to False if you don't want to save figures.
+# This should probably be False for you.
+SaveFigures=True
+
